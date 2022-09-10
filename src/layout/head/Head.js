ebd8348c0d516c1c61ed8dc3190607863876adf7
AnandPatel <91819398+AnandPatel404@users.noreply.github.com>
2022-09-10 13:02:01 +0530

    small title change

--- src/layout/head/Head.js
+++ src/layout/head/Head.js
@@ -2,10 +2,10 @@ import React from "react";
 import { Helmet } from "react-helmet";
 
 const Head = ({ ...props }) => {
-  return (
-    <Helmet>
-      <title>{props.title ? props.title + " | " : null} DashLite React Admin Template</title>
-    </Helmet>
-  );
+	return (
+		<Helmet>
+			<title>{props.title ? props.title + " | " : null} Shorterme - Link shorter</title>
+		</Helmet>
+	);
 };
 export default Head;
