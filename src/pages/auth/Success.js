61a6c384994ab8ff6f8ec6c4fecc152925d36299
AnandPatel <91819398+AnandPatel404@users.noreply.github.com>
2022-09-11 10:00:22 +0530

    success page change

--- src/pages/auth/Success.js
+++ src/pages/auth/Success.js
@@ -4,7 +4,8 @@ import React from "react";
 import PageContainer from "../../layout/page-container/PageContainer";
 import Head from "../../layout/head/Head";
 import AuthFooter from "./AuthFooter";
-import { Block, BlockContent, BlockDes, BlockHead, BlockTitle } from "../../components/Component";
+import { Block, BlockContent, BlockDes, BlockHead, BlockTitle, Button } from "../../components/Component";
+import { Link } from "react-router-dom";
 import { Row, Col } from "reactstrap";
 import r from "../../images/svg/Completed.svg";
 
@@ -15,12 +16,12 @@ const Success = () => {
 			<PageContainer>
 				<Row>
 					<Col xl="7">
-						<div className="d-md-flex justify-content-md-center align-items-md-center">
+						<div className="d-md-flex justify-content-md-center align-items-md-center mt-lg-4">
 							<img src={r} alt="" width={600} />
 						</div>
 					</Col>
-					<Col xl="5">
-						<Block className="nk-auth-body py-5 my-4 wide-s text-center d-flex justify-content-center">
+					<Col xl="5" className="d-flex align-items-center">
+						<Block className="nk-auth-body wide-s">
 							{/* <div className="brand-logo pb-5">
 								<Link to={`${process.env.PUBLIC_URL}/`} className="logo-link">
 									<img className="logo-light logo-img logo-img-lg" src={Logo} alt="logo" />
@@ -29,12 +30,21 @@ const Success = () => {
 							</div> */}
 							<BlockHead>
 								<BlockContent>
-									<BlockTitle tag="h4">Thank you for submitting form</BlockTitle>
+									<BlockTitle tag="h2">Thank you for choosing us</BlockTitle>
 									<BlockDes className="text-success">
-										<p>You can now sign in with your new password</p>
+										<p>You can now sign in with your email and password</p>
 									</BlockDes>
 								</BlockContent>
 							</BlockHead>
+							<div className="form-note-s2 text-center pt-4">
+								<Link
+									className="btn-block py-1 btn-primary"
+									to={`${process.env.PUBLIC_URL}/auth-login`}
+									style={{ borderRadius: 5 + "px" }}
+								>
+									Login
+								</Link>
+							</div>
 						</Block>
 					</Col>
 				</Row>
