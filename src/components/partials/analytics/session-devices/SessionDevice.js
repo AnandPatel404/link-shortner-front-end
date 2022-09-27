f496e271f0916eeedbb120caf27b1e55ecb002b6
AnandPatel <91819398+AnandPatel404@users.noreply.github.com>
2022-09-27 23:12:50 +0530

    bug fixed

--- src/components/partials/analytics/session-devices/SessionDevice.js
+++ src/components/partials/analytics/session-devices/SessionDevice.js
@@ -112,7 +112,7 @@ const SessionDevice = () => {
 					<div className="device-status-data">
 						<Icon style={{ color: "#798bff" }} name="monitor"></Icon>
 						<div className="title">Desktop</div>
-						<div className="amount"> {chart.android}%</div>
+						<div className="amount"> {chart.desktop}%</div>
 						{/* <div className="change up text-danger">
 							<Icon name="arrow-long-up"></Icon>
 							{sessionDevice === "7" ? "2.5" : sessionDevice === "15" ? "4.5" : "10.5"}%
@@ -121,7 +121,7 @@ const SessionDevice = () => {
 					<div className="device-status-data">
 						<Icon style={{ color: "#baaeff" }} name="mobile"></Icon>
 						<div className="title">Mobile</div>
-						<div className="amount"> {chart.desktop}%</div>
+						<div className="amount"> {chart.android}%</div>
 						{/* <div className="change up text-danger">
 							<Icon name="arrow-long-up"></Icon>
 							{sessionDevice === "7" ? "12.5" : sessionDevice === "15" ? "114.5" : "110.5"}%
