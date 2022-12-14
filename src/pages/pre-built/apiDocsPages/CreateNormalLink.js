64a18f580b097dc2d356d66862922e12fa33af6a
AnandPatel <91819398+AnandPatel404@users.noreply.github.com>
2022-12-14 13:03:52 +0530

    create normal link

--- src/pages/pre-built/apiDocsPages/CreateNormalLink.js
+++ src/pages/pre-built/apiDocsPages/CreateNormalLink.js
@@ -4,7 +4,9 @@ import Content from "../../../layout/content/Content";
 
 import Head from "../../../layout/head/Head";
 
-// import { FormGroup, Row, Col, Form, Spinner } from "reactstrap";
+import { Row, Col } from "reactstrap";
+
+import carbon from "../../../Landing/assets/img/carbon.png";
 
 import { Block, BlockHead, BlockHeadContent, BlockTitle, Button, Icon, BlockBetween } from "../../../components/Component";
 
@@ -16,10 +18,16 @@ function CreateNormalLink({ sm, updateSm }) {
 				<BlockHead size="lg" wide="sm">
 					<BlockBetween>
 						<BlockHeadContent>
-							<BlockTitle tag="h2" className="fw-normal">
+							<BlockTitle tag="h2" className="fw-normal align-items-center">
 								Create a Normal Link With Api
 							</BlockTitle>
 						</BlockHeadContent>
+						<div>
+							<p>API Method</p>
+							<Button type="button" className="btn btn-primary btn-dim">
+								Post
+							</Button>
+						</div>
 						<BlockHeadContent className="align-self-start d-lg-none">
 							<Button className={`toggle btn btn-icon btn-trigger mt-n1 ${sm ? "active" : ""}`} onClick={() => updateSm(!sm)}>
 								<Icon name="menu-alt-r"></Icon>
@@ -28,7 +36,26 @@ function CreateNormalLink({ sm, updateSm }) {
 					</BlockBetween>
 				</BlockHead>
 
-				<Block size="lg"></Block>
+				<Block size="lg">
+					<Row className="g-3 d-flex justify-content-center align-items-center">
+						<Col lg="12">
+							<div className="d-flex flex-row justify-content-between">
+								<div className="w-100">
+									<p>URL</p>
+									<div className="font-weight-bold p-3" style={{ backgroundColor: "#fef8e4" }}>
+										https://shorterme.in/v1/api/links
+									</div>
+								</div>
+								<div style={{ cursor: "pointer" }}>
+									<Icon name="check-round-cut" className="h4"></Icon>
+								</div>
+							</div>
+						</Col>
+						<Col lg="10">
+							<img src={carbon} alt="" />
+						</Col>
+					</Row>
+				</Block>
 			</Content>
 		</React.Fragment>
 	);
